acd7fb2d8e2712cdce072a1ebe3722697d46ece7
Cjavierarias <cjavierarias@gmail.com>
2025-12-05 11:12:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,5 @@
 const CACHE_NAME = 'welding-ar-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const urlsToCache = ['./'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -15,35 +11,6 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(response => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then(response => response || fetch(event.request))
   );
 });
